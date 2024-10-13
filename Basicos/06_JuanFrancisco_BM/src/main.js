fa51472f0c5eb684c5581db368291ab858ec8ac7
Isaías Fernández Lozano <isaiasfl@gmail.com>
2024-10-13 23:12:08 +0200

    Entrega de relacion-1-y-2-javascript-dwec-m-Jfranbm04

---
+++ Basicos/06_JuanFrancisco_BM/src/main.js
@@ -0,0 +1,14 @@
+
+
+// --------- EJERCICIO 6 ---------
+/**
+ * Dado un array de números, usa forEach para sumar todos los números.
+ */
+
+import { sumaNum } from "./helpers/script";
+
+const numerosForEach = [2, 4, 6, 8];
+
+// Comprobación 
+console.log(sumaNum(numerosForEach)); // 20
+console.log(sumaNum()); // El argumento no es un array
